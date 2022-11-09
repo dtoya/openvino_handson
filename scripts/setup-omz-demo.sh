e0bea8a95031962e232c207a19539d5066e0ec78
dtoya <80666145+dtoya@users.noreply.github.com>
2022-11-09 10:15:41 +0900

    Update setup-omz-demo.sh

--- scripts/setup-omz-demo.sh
+++ scripts/setup-omz-demo.sh
@@ -1,5 +1,5 @@
 git clone --recurse-submodules https://github.com/openvinotoolkit/open_model_zoo.git
-sudo apt -y install python3-opencv build-essential libopencv-dev
+sudo apt -y install python3-opencv
 python3 -m venv venv/omz_demo
 . venv/omz_demo/bin/activate
 pip install -U pip
@@ -7,6 +7,7 @@ pip install openvino
 pip install -r open_model_zoo/demos/requirements.txt
 pip install open_model_zoo/demos/common/python/
 deactivate
+sudo apt -y install cmake build-essential libopencv-dev
 . openvino_2022/setupvars.sh
 chmod +x open_model_zoo/demos/build_demos.sh
 open_model_zoo/demos/build_demos.sh -b=./omz_demos_build

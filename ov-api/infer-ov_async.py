a735da33d9093c7ab8078501a3740b878b5d8478
Daisuke Toya <dtoya2020@gmail.com>
2022-11-11 12:41:38 +0900

    Add OpenVINO API samples

---
+++ ov-api/infer-ov_async.py
@@ -0,0 +1,84 @@
+# Reference: https://github.com/openvinotoolkit/openvino_notebooks/blob/main/notebooks/101-tensorflow-to-openvino/101-tensorflow-to-openvino.ipynb
+import time
+from pathlib import Path
+
+import cv2
+import numpy as np
+from openvino.runtime import Core, Layout, Type, AsyncInferQueue
+from openvino.preprocess import PrePostProcessor, ResizeAlgorithm, ColorFormat
+
+nireq=6
+nstreams=6
+result_index = [0] * nireq 
+result_value = [0] * nireq 
+
+def completion_callback(infer_request, ireq):
+    result = infer_request.get_output_tensor()
+    result = np.squeeze(result.data)
+    result_index[ireq] = np.argmax(result)
+    result_value[ireq] = result[result_index[ireq]] 
+
+core = Core()
+model = core.read_model("v3-small_224_1.0_float.xml")
+
+# The MobileNet network expects images in RGB format
+image_orig = cv2.imread(filename="coco.jpg")
+
+# Transpose image to network input shape
+input_tensor = np.expand_dims(image_orig, 0)
+
+ppp = PrePostProcessor(model)
+ppp.input().tensor() \
+    .set_shape(input_tensor.shape) \
+    .set_element_type(Type.u8) \
+    .set_color_format(ColorFormat.BGR) \
+    .set_layout(Layout('NHWC'))  
+ppp.input().preprocess() \
+    .convert_color(ColorFormat.RGB) \
+    .resize(ResizeAlgorithm.RESIZE_LINEAR) \
+    .convert_element_type().mean(127.5).scale(127.5)
+ppp.input().model().set_layout(Layout('NHWC'))
+ppp.output().tensor().set_element_type(Type.f32)
+model = ppp.build()
+
+compiled_model = core.compile_model(model=model, device_name="CPU", config={"NUM_STREAMS": nstreams})
+infer_queue = AsyncInferQueue(compiled_model, nireq)
+infer_queue.set_callback(completion_callback)
+
+input_key = compiled_model.input(0)
+output_key = compiled_model.output(0)
+network_input_shape = input_key.shape
+
+infer_queue.start_async({0: input_tensor}, 0)
+infer_queue.wait_all()
+
+# Convert the inference result to a class name.
+imagenet_classes = open("imagenet_2012.txt").read().splitlines()
+
+# The model description states that for this model, class 0 is background,
+# so we add background at the beginning of imagenet_classes
+imagenet_classes = ['background'] + imagenet_classes
+
+print(f"Result: {imagenet_classes[result_index[0]]} {result_value[0]}")
+
+num_images = 1000
+
+start = time.perf_counter()
+
+for _ in range(num_images):
+    input_tensor = np.expand_dims(image_orig, 0)
+    ireq_idle = infer_queue.get_idle_request_id()
+    infer_queue.start_async({0: input_tensor}, ireq_idle)
+
+end = time.perf_counter()
+time_ir = end - start
+
+print(
+    f"IR model in Inference Engine/CPU: {time_ir/num_images:.4f} "
+    f"seconds per image, FPS (includes pre-process): {num_images/time_ir:.2f}"
+)
+
+cv2.imshow('Image', image_orig)
+cv2.waitKey()
+
+

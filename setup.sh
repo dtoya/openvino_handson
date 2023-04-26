7fb256d88fe3e2f6934b944de3e18c9f02dfbfed
Daisuke Toya <dtoya2020@gmail.com>
2023-04-26 23:03:20 +0900

    Updated setup.sh for release 2022.3

--- setup.sh
+++ setup.sh
@@ -3,13 +3,14 @@ sudo apt update
 sudo apt -y install git python3-venv
 
 rm -rf $ARCHIVE_NAME.tgz* openvino_2022
-ARCHIVE_NAME=l_openvino_toolkit_ubuntu20_2022.2.0.7713.af16ea1d79a_x86_64
-wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2022.2/linux/$ARCHIVE_NAME.tgz
-wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2022.2/linux/$ARCHIVE_NAME.tgz.sha256
+ARCHIVE_NAME=l_openvino_toolkit_ubuntu20_2022.3.0.9052.9752fafe8eb_x86_64
+wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2022.3/linux/$ARCHIVE_NAME.tgz
+wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2022.3/linux/$ARCHIVE_NAME.tgz.sha256
 sha256sum -c $ARCHIVE_NAME.tgz.sha256
-tar xvzf $ARCHIVE_NAME.tgz 
+tar xvzf $ARCHIVE_NAME.tgz
 mv $ARCHIVE_NAME openvino_2022
-rm -rf $ARCHIVE_NAME.tgz* 
+rm -rf $ARCHIVE_NAME.tgz*
+sudo -E openvino_2022/install_dependencies/install_openvino_dependencies.sh
 
 sudo apt update
 sudo apt -y install curl

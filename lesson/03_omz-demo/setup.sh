8b0302efac0971b6af080e786c1beeeb70626658
dtoya <80666145+dtoya@users.noreply.github.com>
2023-04-28 12:11:59 +0900

    Update setup.sh

--- lesson/03_omz-demo/setup.sh
+++ lesson/03_omz-demo/setup.sh
@@ -22,3 +22,8 @@ wget -P data  https://storage.openvinotoolkit.org/data/test_data/videos/car-dete
 wget -P data https://storage.openvinotoolkit.org/data/test_data/videos/face-demographics-walking.mp4 
 wget -P data https://assets.amazon.science/ef/0b/234f82204da385f4893a150d7e34/sample01-orig.wav -O data/sample01_wind_noise.wav
 wget -P data https://storage.openvinotoolkit.org/data/test_data/images/car_1.bmp 
+
+. ../../openvino_2022/setupvars.sh
+chmod +x open_model_zoo/demos/build_demos.sh
+open_model_zoo/demos/build_demos.sh -b=./omz_demos_build
+cp -r omz_demos_build/intel64/Release/ bin

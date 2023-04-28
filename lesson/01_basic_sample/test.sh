36373968da5abd85baf83335e50ea1b760448856
dtoya <80666145+dtoya@users.noreply.github.com>
2023-04-28 12:04:33 +0900

    Update test.sh

--- lesson/01_basic_sample/test.sh
+++ lesson/01_basic_sample/test.sh
@@ -2,6 +2,7 @@
 python ../../openvino_2022/samples/python/hello_classification/hello_classification.py public/googlenet-v1/FP32/googlenet-v1.xml car_1.bmp CPU
 deactivate
 
+. ../../openvino_2022/setupvars.sh 
 mkdir -p build
 cd build
 cmake ..
